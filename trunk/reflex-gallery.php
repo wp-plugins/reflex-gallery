792f5c333b9ba2d23a65cedc921786310b2b5957
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-11-12 03:35:04 +0000

    version 1.2 fixes a bug that was causing blank images to be added to a gallery when switching galleries.
    git-svn-id: https://plugins.svn.wordpress.org/reflex-gallery@623866 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/reflex-gallery.php
+++ trunk/reflex-gallery.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package ReFlex_Gallery
- * @version 1.1
+ * @version 1.2
  */
 /*
 Plugin Name: ReFlex Gallery
 Plugin URI: http://wordpress-photo-gallery.com/
 Description: Wordpress Plugin for creating responsive image galleries. By: HahnCreativeGroup
 Author: HahnCreativeGroup
-Version: 1.1
+Version: 1.2
 Author URI: http://labs.hahncreativegroup.com/
 */
 if (!class_exists("ReFlex_Gallery")) {
