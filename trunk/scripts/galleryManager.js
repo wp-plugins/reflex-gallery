4086fab7927cc0567c8bcaf3aa0d83e4a22413ff
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-12 03:19:13 +0000

    Version 1.3 - bug fix to prevent gallery from jumping when clicked.
    git-svn-id: https://plugins.svn.wordpress.org/reflex-gallery@637426 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/scripts/galleryManager.js
+++ trunk/scripts/galleryManager.js
@@ -43,7 +43,7 @@ jQuery(window).load(function() {
 						
 				 });
 				  
-				    var gallery = "<span class='gallery'><a href='#' onclick=\"var images=["+galleryImgs+"]; var titles=["+imageTitles+"]; var descriptions=["+imageDesc+"]; jQuery.prettyPhoto.open(images,titles,descriptions);\"><img src="+galleryImgs[0]+""+width+height+" /></a></span>";
+				    var gallery = "<span class='gallery'><a style='cursor: pointer;' onclick=\"var images=["+galleryImgs+"]; var titles=["+imageTitles+"]; var descriptions=["+imageDesc+"]; jQuery.prettyPhoto.open(images,titles,descriptions);\"><img src="+galleryImgs[0]+""+width+height+" /></a></span>";
 				
 					pGalleries.push(gallery);
 				
