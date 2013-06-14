4b6939f471ce743276c9c0670b5b4f98003c1ae7
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-14 05:01:30 +0000

    back-up of version 1.4.7
    git-svn-id: https://plugins.svn.wordpress.org/reflex-gallery@726522 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.4.7/admin/overview.php
@@ -0,0 +1,147 @@
+<?php
+if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die('You are not allowed to call this page directly.'); }
+
+if(isset($_POST['galleryId'])) {
+	if(check_admin_referer('reflex_gallery','reflex_gallery')) {
+	  $this->reflexdb->deleteGallery(intval($_POST['galleryId']));
+		  
+	  ?>  
+	  <div class="updated"><p><strong><?php _e('Gallery has been deleted.', 'reflex-gallery'); ?></strong></p></div>  
+	  <?php	
+	}
+}
+
+$galleryResults = $this->reflexdb->getGalleries();
+
+if (isset($_POST['defaultSettings'])) {
+	if(check_admin_referer('reflex_gallery','reflex_gallery')) {
+	  $temp_defaults = get_option('reflex_gallery_options');
+	  $temp_defaults[1]['thumbnail_width'] = $_POST['default_width'];
+	  $temp_defaults[1]['thumbnail_height'] = $_POST['default_height'];
+	  
+	  update_option('reflex_gallery_options', $temp_defaults);
+	  ?>  
+	  <div class="updated"><p><strong><?php _e('Gallery options have been updated.', 'reflex-gallery'); ?></strong></p></div>  
+	  <?php
+	}
+}
+$default_options = get_option('reflex_gallery_options');
+?>
+<div class='wrap'>
+<h2>ReFlex Gallery</h2>
+<p><?php _e('This is a listing of all galleries', 'reflex-gallery'); ?></p>
+    <table class="widefat post fixed" id="galleryResults" cellspacing="0">
+    	<thead>
+        <tr>
+        	<th><?php _e('Gallery Name', 'reflex-gallery'); ?></th>
+            <th><?php _e('Gallery Short Code', 'reflex-gallery'); ?></th>
+            <th><?php _e('Description', 'reflex-gallery'); ?></th>
+            <th width="136"></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+        	<th><?php _e('Gallery Name', 'reflex-gallery'); ?></th>
+            <th><?php _e('Gallery Short Code', 'reflex-gallery'); ?></th>
+            <th><?php _e('Description', 'reflex-gallery'); ?></th>
+            <th></th>
+        </tr>
+        </tfoot>
+        <tbody>
+        	<?php foreach($galleryResults as $gallery) { ?>				
+            <tr>
+            	<td><?php _e($gallery->name); ?></td>
+                <td><input type="text" size="40" value="[ReflexGallery id='<?php _e($gallery->Id); ?>']" /></td>
+                <td><?php _e($gallery->description); ?></td>
+                <td class="major-publishing-actions">
+                <form name="delete_gallery_<?php _e($gallery->Id); ?>" method ="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+                	<?php wp_nonce_field('reflex_gallery', 'reflex_gallery'); ?>
+                	<input type="hidden" name="galleryId" value="<?php _e($gallery->Id); ?>" />
+                    <input type="submit" name="Submit" class="button-primary" value="<?php _e('Delete Gallery', 'reflex-gallery'); ?>" />
+                </form>
+                </td>
+            </tr>
+			<?php } ?>
+        </tbody>
+     </table>
+     <br />
+     <h3><?php _e('Default Options', 'reflex-gallery'); ?></h3>
+     <form name="save_default_settings" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">
+     <?php wp_nonce_field('reflex_gallery', 'reflex_gallery'); ?>
+     <table class="widefat post fixed" cellspacing="0">
+     	<thead>
+        	<th><?php _e('Attribute', 'reflex-gallery'); ?></th>
+            <th><?php _e('Default Value', 'reflex-gallery'); ?></th>
+            <th><?php _e('Description', 'reflex-gallery'); ?></th>
+        </thead>
+        <tfoot>
+        	<th><?php _e('Attribute', 'reflex-gallery'); ?></th>
+            <th><?php _e('Default Value', 'reflex-gallery'); ?></th>
+            <th><?php _e('Description', 'reflex-gallery'); ?></th>
+        </tfoot>
+        <tbody>
+        	<tr>
+            	<td><?php _e('Default Thumbnail Width', 'reflex-gallery'); ?></td>
+                <td><input name="default_width" id="default_width" value="<?php _e($default_options[1]['thumbnail_width']); ?>" /> px</td>
+                <td><?php _e('This is the default width (in pixels) of all of the gallery thumbnail images.<br />(This property can be overwritten when creating individual galleries.)', 'reflex-gallery'); ?></td>
+            </tr>
+            <tr>
+            	<td><?php _e('Default Thumbnail Height', 'reflex-gallery'); ?></td>
+                <td><input name="default_height" id="default_height" value="<?php _e($default_options[1]['thumbnail_height']); ?>" /> px</td>
+                <td><?php _e('This is the default height (in pixels) of all of the gallery thumbnail images.<br />(This property can be overwritten when creating individual galleries.)', 'reflex-gallery'); ?></td>
+            </tr>
+            <tr>
+            	<td>                
+                	<input type="hidden" name="defaultSettings" value="true" />
+                    <input type="submit" name="Submit" class="button-primary" value="<?php _e('Save', 'reflex-gallery'); ?>" />                
+                </td>
+                <td></td>
+                <td>
+            </tr>
+        </tbody>
+     </table>
+     </form>
+     <br /><br />
+     <table class="widefat post fixed">
+    	<thead>
+        <tr>
+        	<th><em>Please consider making a donatation for the continued development of this plugin. Thanks.</em></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+        	<th></th>
+        </tr>
+        </tfoot>
+        <tbody>        				
+            <tr>
+            <td><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&amp;hosted_button_id=BD7VZR88K9DB4" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" alt="PayPal - The safer, easier way to pay online!"><img alt="" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></a></td>            
+            </tr>
+            </tbody>
+            </table>
+            <br /><br />
+     <table class="widefat post fixed">
+    	<thead>
+        <tr>
+        	<th><em>Other plugins by <a href="http://labs.hahncreativegroup.com/" target="_blank">HahnCreativeGroup</a></em></th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+        	<th></th>
+        </tr>
+        </tfoot>
+        <tbody>        				
+            <tr>
+            <td>
+            	<ul>
+                	<li><a href="http://wordpress-photo-gallery.com/?src=rg" target="_blank">ReFlex Gallery Pro</li>
+                    <li><a href="http://labs.hahncreativegroup.com/wordpress-plugins/wp-easy-gallery-pro-simple-wordpress-gallery-plugin/?src=rg" target="_blank">WP Easy Gallery Pro</a></li>
+                    <li><a href="http://wordpress.org/extend/plugins/custom-post-donations/" target="_blank">Custom Post Donations</a></li>
+                    <li><a href="http://wordpress.org/extend/plugins/wp-translate/" target="_blank">WP Translate</a></li>
+                </ul>
+            </td>            
+            </tr>
+            </tbody>
+            </table>
+</div>
---
+++ tags/1.4.7/reflex-gallery.php
@@ -0,0 +1,178 @@
+<?php
+/**
+ * @package ReFlex_Gallery
+ * @version 1.4.7
+ */
+/*
+Plugin Name: ReFlex Gallery
+Plugin URI: http://wordpress-photo-gallery.com/
+Description: Wordpress Plugin for creating responsive image galleries. By: HahnCreativeGroup
+Author: HahnCreativeGroup
+Version: 1.4.7
+Author URI: http://labs.hahncreativegroup.com/
+*/
+if (!class_exists("ReFlex_Gallery")) {
+	class ReFlex_Gallery {
+		
+		//Constructor
+		public function __construct() {
+			$this->plugin_name = plugin_basename(__FILE__);		
+			$this->define_constants();
+			$this->define_db_tables();			
+			$this->add_gallery_options();
+			$this->reflexdb = $this->create_db_conn();
+			
+			register_activation_hook( $this->plugin_name,  array(&$this, 'create_db_tables') );
+			add_action('init', array($this, 'create_textdomain'));
+			add_action('wp_enqueue_scripts', array($this, 'add_gallery_scripts'));
+			
+			add_action( 'admin_init', array($this,'gallery_admin_init') );
+			add_action( 'admin_menu', array($this, 'add_gallery_admin_menu') );
+			
+			add_shortcode( 'ReflexGallery', array($this, 'gallery_shortcode_handler') );
+		}
+		
+		//Define textdomain
+		public function create_textdomain() {
+			$plugin_dir = basename(dirname(__FILE__));
+			load_plugin_textdomain( 'reflex-gallery', false, $plugin_dir.'/lib/languages' );
+		}
+		
+		//Define constants
+		public function define_constants() {
+			if ( ! defined( 'RESPONSIVEFLEXIGALLERY_PLUGIN_BASENAME' ) )
+				define( 'RESPONSIVEFLEXIGALLERY_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+		
+			if ( ! defined( 'RESPONSIVEFLEXIGALLERY_PLUGIN_NAME' ) )
+				define( 'RESPONSIVEFLEXIGALLERY_PLUGIN_NAME', trim( dirname( RESPONSIVEFLEXIGALLERY_PLUGIN_BASENAME ), '/' ) );
+			
+			if ( ! defined( 'RESPONSIVEFLEXIGALLERY_PLUGIN_DIR' ) )
+				define( 'RESPONSIVEFLEXIGALLERY_PLUGIN_DIR', WP_PLUGIN_DIR . '/' . RESPONSIVEFLEXIGALLERY_PLUGIN_NAME );
+		}
+		
+		//Define DB tables
+		public function define_db_tables() {
+			global $wpdb;
+			
+			$wpdb->reflexGalleries = $wpdb->prefix . 'reflex_gallery';
+			$wpdb->reflexImages = $wpdb->prefix . 'reflex_gallery_images';
+		}
+		
+		//Create DB tables
+		public function create_db_tables() {
+			include_once (dirname (__FILE__) . '/lib/install-db.php');
+			
+			install_db();
+		}
+		
+		public function create_db_conn() {
+			require('lib/db-class.php');
+			$reflexdb = ReflexDB::getInstance();
+			return $reflexdb;
+		}
+		
+		//Add gallery options
+		public function add_gallery_options() {
+			$gallery_options = array(
+				'thumbnail_width'  => 'auto',
+				'thumbnail_height' => 'auto'																	 
+			);
+			
+			add_option('reflex_gallery_options', array($this, $gallery_options));
+		}
+		
+		//Add gallery scripts
+		public function add_gallery_scripts() {			
+			wp_enqueue_script('jquery');
+			wp_register_script('jquery_migrate', WP_PLUGIN_URL.'/reflex-gallery/scripts/jquery-migrate.js', array('jquery'), null);
+			wp_enqueue_script('jquery_migrate');
+			wp_register_script('flexSlider', WP_PLUGIN_URL.'/reflex-gallery/scripts/flexslider/jquery.flexslider-min.js', array('jquery'));
+			wp_register_script('prettyPhoto', WP_PLUGIN_URL.'/reflex-gallery/scripts/prettyphoto/jquery.prettyPhoto.js', array('jquery'));
+			wp_register_script('galleryManager', WP_PLUGIN_URL.'/reflex-gallery/scripts/galleryManager.min.js', array('flexSlider', 'prettyPhoto', 'jquery'));
+			wp_enqueue_script('flexSlider');
+			wp_enqueue_script('prettyPhoto');
+			wp_enqueue_script('galleryManager');
+			wp_register_style('flexSlider_stylesheet', WP_PLUGIN_URL.'/reflex-gallery/scripts/flexslider/flexslider.css');
+			wp_register_style('prettyPhoto_stylesheet', WP_PLUGIN_URL.'/reflex-gallery/scripts/prettyphoto/prettyPhoto.css');
+			wp_enqueue_style('flexSlider_stylesheet');
+			wp_enqueue_style('prettyPhoto_stylesheet');
+		}
+				
+		//Admin Section - register scripts and styles
+		public function gallery_admin_init() {
+			wp_register_style( 'table_pager_stylesheet', WP_PLUGIN_URL.'/reflex-gallery/admin/scripts/TablePagination/tablePager.css');
+			wp_register_style( 'prettyPhoto_admin_stylesheet', WP_PLUGIN_URL.'/reflex-gallery/scripts/prettyphoto/prettyPhoto.css');
+		}
+		
+		public function add_default_media_uploader() {
+			wp_enqueue_script('media-upload');
+			wp_enqueue_script('thickbox');
+			wp_register_script('easy-gallery-uploader', WP_PLUGIN_URL.'/reflex-gallery/admin/scripts/MediaUpload/image-uploader.js', array('jquery','media-upload','thickbox'));
+			wp_enqueue_script('easy-gallery-uploader');
+			wp_enqueue_style('thickbox');
+		}
+		
+		public function reflex_gallery_admin_style_load() {
+			wp_enqueue_style('table_pager_stylesheet');
+		}
+		
+		public function reflex_gallery_admin_style_images() {
+			wp_enqueue_style('table_pager_stylesheet');
+			wp_enqueue_style('prettyPhoto_admin_stylesheet');
+		}
+		
+		//Create Admin Menu
+		public function add_gallery_admin_menu() {
+			$overview = add_menu_page(__('ReFlex Gallery','reflex-gallery'), __('ReFlex Gallery','reflex-gallery'), 'manage_options', 'reflex-gallery-admin', array($this, 'add_overview'));
+			$add_gallery = add_submenu_page('reflex-gallery-admin', __('ReFlex Gallery >> Add Gallery','reflex-gallery'), __('Add Gallery','reflex-gallery'), 'manage_options', 'add-gallery', array($this, 'add_gallery'));
+			$edit_gallery = add_submenu_page('reflex-gallery-admin', __('ReFlex Gallery >> Edit Gallery','reflex-gallery'), __('Edit Gallery','reflex-gallery'), 'manage_options', 'edit-gallery', array($this, 'edit_gallery'));
+			$add_images = add_submenu_page('reflex-gallery-admin', __('ReFlex Gallery >> Add Images','reflex-gallery'), __('Add Images','reflex-gallery'), 'manage_options', 'add-images', array($this, 'add_images'));	
+			
+			add_action('admin_print_styles-'.$overview, array($this, 'reflex_gallery_admin_style_load'));
+			add_action('admin_print_styles-'.$edit_gallery, array($this, 'reflex_gallery_admin_style_load'));
+			add_action('admin_print_styles-'.$add_images, array($this, 'reflex_gallery_admin_style_images'));
+			add_action('admin_print_styles-'.$add_images, array($this, 'add_default_media_uploader'));
+		}
+		
+		//Create Admin Pages
+		public function add_overview() {			
+			include("admin/overview.php");
+		}
+		
+		public function add_gallery() {
+			include("admin/add-gallery.php");	
+		}
+		public function edit_gallery() {
+			include("admin/edit-gallery.php");	
+		}
+		
+		public function add_images() {
+			include("admin/add-images.php");
+		}	
+		
+		//Create gallery
+		public function create_gallery($galleryId) {
+			require_once('lib/gallery-class.php');			
+			global $responsiveGallery;
+			
+			if (class_exists('ResponsiveGallery')) {
+				$responsiveGallery = new ResponsiveGallery($galleryId, $this->reflexdb);
+				return $responsiveGallery->render();
+			}
+			else {
+				return "Gallery not found.";	
+			}	
+		}
+		
+		//Create Short Code
+		public function gallery_shortcode_handler($atts) {
+			return $this->create_gallery($atts['id']);
+		}
+	}
+}
+
+if (class_exists("ReFlex_Gallery")) {
+    global $ob_ReFlex_Gallery;
+	$ob_ReFlex_Gallery = new ReFlex_Gallery();
+}
+?>
