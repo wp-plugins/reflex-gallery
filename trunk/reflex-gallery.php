8b076cac7eb39af14b5a321d9e84196e5e1abc55
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-11-30 19:22:35 +0000

    Version 2.2 adds enhanced image editing functionality
    git-svn-id: https://plugins.svn.wordpress.org/reflex-gallery@812967 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/reflex-gallery.php
+++ trunk/reflex-gallery.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package ReFlex_Gallery
- * @version 2.1
+ * @version 2.2
  */
 /*
 Plugin Name: ReFlex Gallery
 Plugin URI: http://wordpress-photo-gallery.com/
 Description: Wordpress Plugin for creating responsive image galleries. By: HahnCreativeGroup
 Author: HahnCreativeGroup
-Version: 2.1
+Version: 2.2
 Author URI: http://labs.hahncreativegroup.com/
 */
 if (!class_exists("ReFlex_Gallery")) {
